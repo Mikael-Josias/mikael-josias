138a18f2e358e41e164c2b30a3b4e686a76060bd
Mikael Josias <mikael.josias01@gmail.com>
2025-02-14 10:11:06 -0300

    style(header): change header color on dark mode mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
 
 
   return (
-    <header className="w-full border-b-[1px] border-black/25 dark:border-white/25 py-6 bg-wheat-600 dark:bg-transparent md:bg-transparent">
+    <header className="w-full border-b-[1px] border-black/25 dark:border-white/25 py-6 bg-wheat-600 dark:bg-black/25 dark:md:bg-transparent md:bg-transparent">
       <div className="flex justify-between items-center mx-auto max-w-3xl px-10 md:px-0">
         <h1 className="font-amaranth text-2xl text-cement-600 dark:text-white select-none pointer-events-none md:text-3xl">Mikael Josias</h1>
         <ToggleTheme />
