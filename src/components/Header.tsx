8498232eaef00ce7ed8b86db0aad21dc702db68a
mikael josias <mikaeljosias2002@gmail.com>
2025-03-21 22:11:31 -0300

    style(header): redesign header border and add transition animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,11 @@ import ToggleTheme from "./ToggleTheme";
 
 function Header() {
   return (
-    <header className="w-full md:border-b-[1px] md:border-black/25 dark:md:border-white/25 py-6 bg-wheat-600 dark:bg-black/25 dark:md:bg-transparent md:bg-transparent">
-      <div className="flex justify-between items-center mx-auto max-w-3xl px-5 md:px-0">
-        <h1 className="font-amaranth text-2xl text-cement-600 dark:text-white select-none pointer-events-none md:text-3xl">
-          Mikael Josias
-        </h1>
-        <ToggleTheme />
-      </div>
+    <header className="w-full md:w-[720px] flex flex-row items-center justify-between p-6 md:px-0 border-b-1 border-b-black/20 dark:border-b-white/20 transition-all">
+      <h1 className="font-amaranth text-2xl md:text-3xl text-cement-600 dark:text-white transition-all select-none pointer-events-none">
+        Mikael Josias
+      </h1>
+      <ToggleTheme />
     </header>
   );
 }
