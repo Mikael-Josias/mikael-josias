c3e08f43dc20378173858514e2112449263e3b04
mikael josias <mikaeljosias2002@gmail.com>
2025-03-22 23:25:16 -0300

    style(hero): change hero dimensions and add animation

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,26 +1,31 @@
-import Photo1 from "../assets/Photo1.png"
-import SocialBar from "./SocialBar"
+import Photo1 from "../assets/Photo1.png";
+import SocialBar from "./SocialBar";
 
 function HeroSection() {
   return (
-    <section className="flex flex-col-reverse md:flex-row items-start w-full md:max-w-3xl gap-3 md:gap-10 py-10 px-5 md:px-0">
-      <div className="flex flex-col gap-3 md:gap-6">
-        <h2 className="font-amaranth text-2xl md:text-5xl text-cement-600 dark:text-white ">Hello! 👋</h2>
-        <span className="text-sm md:text-lg font-light text-cement-500 dark:text-white">
-          <p className="mb-1.5">
-            I’m a 23-year-old software developer from Brazil 🇧🇷.
-          </p>
+    <section className="w-full md:w-[720px] py-12 px-6 md:px-0 flex flex-col-reverse md:flex-row gap-4">
+      <div className="flex flex-col gap-4 md:w-full transition-all">
+        <h2 className="font-amaranth text-4xl font-bold text-cement-600 dark:text-white">
+          Hello!{" "}
+          <span className="hover:animate-waving origin-[70%_70%] inline-block">
+            👋
+          </span>
+        </h2>
+        <span className="font-ubuntu font-light text-base text-cement-500 dark:text-white">
+          <p>I’m a 23-year-old software developer from Brazil 🇧🇷.</p>
           <p>
-            I like playing video games, take care of my plants and develop awesome applications.
+            I like playing video games, take care of my plants and develop
+            awesome applications.
           </p>
         </span>
-        <div>
-          <SocialBar />
-        </div>
+        <SocialBar />
       </div>
-      <img src={Photo1} alt="Mikael Josias photo" className="rounded-3xl max-h-96 md:max-h-60 w-full md:max-w-[300px] md:h-full object-cover select-none pointer-events-none" />
+      <img
+        src={Photo1}
+        className="h-32 md:h-52 object-center object-cover rounded-2xl transition-all"
+      />
     </section>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
