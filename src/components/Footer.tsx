460491380d43d9fa9015f4ffd22dc05d2f4dd369
mikael josias <mikaeljosias2002@gmail.com>
2025-03-25 16:54:03 -0300

    refactor(footer): change footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,11 @@
-import SocialBar from "./SocialBar"
-
 function Footer() {
   return (
-    <footer className="w-full bg-wheat-600 dark:bg-black/25 md:bg-transparent dark:md:bg-transparent py-10 px-5 md:px-0 flex flex-col gap-3 md:gap-6 items-center justify-center md:border-t-[1px] md:border-t-black/25 dark:md:border-t-white/25">
-      <span className="text-sm md:text-base text-cement-500 dark:text-white font-light">Thank you for stopping by! 😊</span>
-      <SocialBar />
+    <footer className="w-full md:w-[720px] p-6 md:px-0 flex items-center justify-center border-t-[1px] border-t-black/25 dark:md:border-t-white/25">
+      <span className="text-sm md:text-base text-cement-500 dark:text-white font-light">
+        Thank you for stopping by! 😊
+      </span>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
